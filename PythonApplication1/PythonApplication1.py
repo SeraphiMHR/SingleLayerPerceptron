88931e4d05daf48961710636aef85196346a7865
Akimov A <akimov1dev@gmail.com>
2019-12-22 09:37:59 +0300

    update

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+training_inputs = np.array([[0,0,1],
+                            [1,1,1],    
+                            [1,0,1],
+                            [0,1,1]])
+
+training_outputs = np.array([[0,1,1,0]]).T
+
+np.random.seed(1)
+synaptic_weights = 2* np.random.random((3,1)) - 1
+
+#Backpropagation
+
+for i in range(20000):
+    input_layer = training_inputs
+    outputs  = sigmoid(np.dot(input_layer,synaptic_weights))
+    error = training_outputs - outputs
+    adjustments = np.dot( input_layer.T, error * (outputs *(1-outputs)) )
+    synaptic_weights +=adjustments
+np.around(outputs)
+
+print("Результат: \n",outputs)
+new_inputs = np.array([1,0,0])
+output = sigmoid(np.dot(new_inputs,synaptic_weights))
+print("\n \n \n",output)
